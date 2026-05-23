6ffbca1448dec74267df3840903fefb53f6abc77
Dany <muhammaddeny000@gmail.com>
2026-05-23 10:21:34 +0700

    fix eror api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://api-absenin-sekolah.onrender.com'
+  baseURL: "https://api-absenin-sekolah.onrender.com/api",
 });
 
 // Interceptor untuk menyisipkan JWT Token otomatis jika sudah login (Untuk request absensi nanti)
